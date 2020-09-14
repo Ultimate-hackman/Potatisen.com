5f9058fdbb3ba38926d539e1689869b516e63b3c
Ultimate-hackman <70284330+Ultimate-hackman@users.noreply.github.com>
2020-09-14 15:51:25 +0200

    yeah nice font bro and icon

--- Javascript3/Printers/daliy_meme.js
+++ Javascript3/Printers/daliy_meme.js
@@ -1,12 +1,15 @@
-let citat = "hunt or be hunted" 
-let transition_citat = "Hey boss!"
+let citat = "God is dead. God remains dead. And we have killed him." 
+meddelande =  "<h1>" + "DAGENS CITAT = " + citat.toUpperCase(); + "</h1>"
+document.write(meddelande);
 
 
+
+
+/*
 let today = new Date();
 let date = today.getDate();
 let countDownDate = (date += -1)
-
-
+let transition_citat = "Hey boss!"
 if (date == countDownDate) {
     meddelande =  "<h1>" + "DAGENS CITAT = " + transition_citat.toUpperCase(); + "</h1>"
     document.write(meddelande);
@@ -18,3 +21,4 @@ if (date == countDownDate) {
     document.write(meddelande);
 }
 
+*/ 
