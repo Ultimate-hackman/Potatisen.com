59e8d0757b551ba059813729dfcf70af20116293
Ultimate-hackman <70284330+Ultimate-hackman@users.noreply.github.com>
2020-10-11 19:54:56 +0200

    temporary fix week

--- Javascript3/Script/week.js
+++ Javascript3/Script/week.js
@@ -1,7 +1,9 @@
 
 function weekFinder() {
     let month = new Date().getMonth();
-    let day = new Date().getDate();
+    let day = new Date().getDate(); 
+
+    day -= 4
 
     let week = Math.ceil(((month * 30.5) + day) / 7) + 1
 
