7dfd5161ded9e46c2dad961cb222f523d906a520
Ultimate-hackman <70284330+Ultimate-hackman@users.noreply.github.com>
2020-10-12 16:06:50 +0200

    lower focal on veckobrev, easy to understand code

--- Javascript3/src/countdown.js
+++ Javascript3/src/countdown.js
@@ -6,10 +6,13 @@ function wencesAttack(date) {
 
         let difference = now - target
 
+        totalHour = Math.floor(difference / (1000 * 60 * 60));
+        totalMinutes = Math.floor((difference / 1000) / 60);
+
         let days = Math.floor(difference / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+        let hours = Math.floor((((difference / 1000) / 60) / 60) - days * 24);
+        let minutes = Math.floor(((difference / 1000) / 60) - totalHour * 60);
+        let seconds = Math.floor((difference / 1000) - totalMinutes * 60);
       
 
          document.getElementById("countdown").innerHTML = 'Online sedan: ' + String(days) + ' dagar, ' + String(hours) + ' timmar, ' + String(minutes) + ' minuter, '+ String(seconds) + ' sekunder' 
@@ -19,3 +22,4 @@ function wencesAttack(date) {
 }
 
 wencesAttack("sep 13, 2020 17:37:25")
+
