2a320e9d043a5fb18ef1ac06fb9a2bc190ec406c
Ultimate-hackman <70284330+Ultimate-hackman@users.noreply.github.com>
2020-10-13 20:14:58 +0200

    fixed minor scaling issue

--- Javascript3/src/countdown.js
+++ Javascript3/src/countdown.js
@@ -8,18 +8,56 @@ function wencesAttack(date) {
 
         totalHour = Math.floor(difference / (1000 * 60 * 60));
         totalMinutes = Math.floor((difference / 1000) / 60);
-
-        let days = Math.floor(difference / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((((difference / 1000) / 60) / 60) - days * 24);
+        totaldays = Math.floor(difference / (1000 * 60 * 60 * 24));
+        totalMonths = Math.floor(difference / (1000 * 60 * 60 * 24 * 31));
+        
+        
+        let days = Math.floor(difference / (1000 * 60 * 60 * 24) - totalMonths);
+        let hours = Math.floor((((difference / 1000) / 60) / 60) - totaldays * 24);
         let minutes = Math.floor(((difference / 1000) / 60) - totalHour * 60);
         let seconds = Math.floor((difference / 1000) - totalMinutes * 60);
-      
 
-         document.getElementById("countdown").innerHTML = 'Online sedan: ' + days + ' dagar, ' + hours + ' timmar, ' + minutes + ' minuter, '+ seconds + ' sekunder' 
+
+
+
+
+        // plural check
+        if(days == 1) {
+            c = " dag, "
+        } else {
+            c = " dagar, "
+        }
+
+        if(hours == 1) {
+            d = " timme, "
+        } else {
+            d = " timmar, "
+        }
+
+        if(minutes == 1) {
+            e = " minut, "
+        } else {
+            e = " minuter, "
+        }
+
+        if(seconds == 1) {
+            f = " sekund, "
+        } else {
+            f = " sekunder, "
+        }
+
+
+        
+
+            document.getElementById("countdown").innerHTML = 'Online sedan: ' + days + c + hours + d + minutes + e + seconds + f
+
+
     }, 1000)
 
 
 }
 
 wencesAttack("sep 13, 2020 18:27:30")
+
+
 
