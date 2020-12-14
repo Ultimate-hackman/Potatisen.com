3aa2f4406653819c1a3a92b67c5d74728d0ab937
Ultimate-hackman <70284330+Ultimate-hackman@users.noreply.github.com>
2020-12-14 11:25:11 +0100

    fixed week count

--- src/lib/time/weekCount.ts
+++ src/lib/time/weekCount.ts
@@ -2,10 +2,8 @@ export default function weekFinder(weekday, hour) {
     let month = new Date().getMonth();
     let day = new Date().getDate();
 
-    let target = [weekday, hour]
-
    
-    day -= target[0] + (target[1]/10)/2.4
+    day -= weekday - (hour/24)
 
     let week = Math.ceil((((month * 30.5) + day)/ 7))
 
