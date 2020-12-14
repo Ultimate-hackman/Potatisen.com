c9aec193935a3e01828914af071762feb4b01023
Ultimate-hackman <70284330+Ultimate-hackman@users.noreply.github.com>
2020-12-14 10:21:03 +0100

    babelrc and button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import Head from 'next/head'
 import styled from "styled-components";
 
-import Title from '../styles/title'
-import GlobalStyle from '../theme/GlobalStyles'
 import Header from '../components/header'
+
+import Title from '../styles/title'
+import Img from '../styles/img'
+import Btn from '../styles/button'
+import GlobalStyle from '../theme/GlobalStyles'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -11,8 +15,13 @@ export default function Home() {
         <GlobalStyle/>
         <Header/>
         <Title>
-          Potatisen.com
+        Välkommen till Potatisen.com!
         </Title>
+        <Img src="https://media.discordapp.net/attachments/688322560957743190/786279776468992071/shrek.png"></Img>
+
+        <Link href="/veckobrev">
+        <Btn>Veckobrev</Btn>
+       </Link>
     </>
   )
 }
