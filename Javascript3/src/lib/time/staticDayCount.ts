fd3f0a8e4f84a75f519ed2043a8939d8f7005a1b
Ultimate-hackman <70284330+Ultimate-hackman@users.noreply.github.com>
2020-12-14 10:06:46 +0100

    Purge

--- Javascript3/src/lib/time/staticDayCount.ts
+++
@@ -1,6 +0,0 @@
-export default function staticDay(date) {
-
-    let totalDays = Math.floor((new Date(date).getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24));
-  
-    return totalDays
-  }
--- Javascript3/src/styles/text.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-const Text = styled.p `
-    text-align: center;
-    font-size: 1.4em;
-`
-
-export default Text
