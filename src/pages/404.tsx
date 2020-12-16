b7c39206fa5f4092fe703ce491fa2fc8f651c37e
Ultimate-hackman <70284330+Ultimate-hackman@users.noreply.github.com>
2020-12-16 18:45:09 +0100

    enkel 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import styled from "styled-components";
+
+import staticDayCount from '../lib/time/staticDayCount'
+import pluralCheck from '../lib/time/pluralCheck'
+
+import GlobalStyle from '../theme/GlobalStyles'
+import Link from 'next/link'
+import Header from '../components/header'
+
+import NewsPad from '../styles/newsPad'
+import Title from '../styles/title'
+import Img from '../styles/img'
+import Btn from '../styles/button'
+
+
+const Question = styled(Img) `
+width: 15%;
+`
+
+export default function Home() {
+  let time = staticDayCount("dec 25, 2020 00:00:00")
+  return (
+    <>
+
+        <GlobalStyle/>
+        <Header/>
+        <Title>
+        404 
+        </Title>
+        <Title sub>
+        Sidan gick inte att hitta 
+        </Title>
+        <Question src="https://cdn.discordapp.com/attachments/688322560957743190/788822693277335552/question.png"></Question>
+
+        <Link href="/veckobrev">
+        <Btn>Hem</Btn>
+       </Link>
+
+       <NewsPad>
+        <h1>Sidan gick inte att hitta </h1>
+       </NewsPad>
+    </>
+  )
+}
