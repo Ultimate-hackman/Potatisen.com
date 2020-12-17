80690ab7268224d30830b5c9c81acbc4def821c1
Ultimate-hackman <70284330+Ultimate-hackman@users.noreply.github.com>
2020-12-17 12:43:44 +0100

    added poopy databse and test days (TERRIBLE PROTOTYPE)

--- src/components/hatches.tsx
+++ src/components/hatches.tsx
@@ -3,12 +3,15 @@ import useDownloadUrl from "../lib/firebase/useDownloadUrl";
 import styled from "styled-components";
 import Title from '../styles/title'
 import { Console } from "console";
+import React, { useState, useEffect } from 'react';
+import firebase from '../lib/firebase/firebase'
 
 const Hatch = styled.div `
 box-shadow: 1px 1px 8px 6px rgba(58, 58, 58, 0.096);
 border-radius: 10px;
 width: 10vw;
 text-align: center;
+background-color: ${props => props.color};
 `
 const HatchClose = styled(Hatch) `
 
@@ -32,12 +35,41 @@ const dataSet = [[2020, 12, 24, "NO", "jul"], [2020, 12, 20, "NO", "Xd poop"], [
 
 
 let day = new Date().getDate()
-let time = new Date()
 
 
 
 
 function HatchMake({ i }) {
+
+const database = firebase.firestore();
+let array: any = new Array()
+const [color, setColor] = useState("white")
+const [info, setInfo] = useState(undefined);
+const [subject, setSubject] = useState(undefined);
+
+
+useEffect(() => {
+  database.collection('prov').get().then((snapshot) =>{
+    snapshot.docs.forEach(doc => {
+        array.push(doc.data().prov)
+
+    })
+  
+
+array.map((_, index) => {
+  if (i === array[index][2]) {
+    setInfo(array[index][4])
+    setSubject(array[index][3])
+    setColor("red")
+
+  } else {
+
+  }
+
+})
+})
+}, []);
+
 
   const monthLenght = monthsLenght[new Date().getMonth()]
 
@@ -45,21 +77,13 @@ function HatchMake({ i }) {
     i -= monthLenght
   }
 
-  let customMessage: string = ""
-  
 
   console.log(new Date().getMonth())
 
 
-  for (let n = 0; n < dataSet.length; n++) {
-    if (i === dataSet[n][2]) {
-      customMessage += String(dataSet[n][4])
-    } else {
-    }
-  }
   
     return (
-      <Hatch>{i} <Alert> {customMessage}</Alert></Hatch>
+      <Hatch color = {color}>{i} <Alert> {info}  </Alert> {subject}</Hatch>
     )
 
 
