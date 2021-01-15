ae21be5160525d6713b34e1bb7361e3077679ac2
Ultimate-hackman <70284330+Ultimate-hackman@users.noreply.github.com>
2021-01-15 22:43:50 +0100

    asdasd

--- src/pages/kalender.tsx
+++ src/pages/kalender.tsx
@@ -96,13 +96,18 @@ export default function Kalender() {
   useEffect(() => {
     setUgg(() => localStorage.getItem('UserUgg') === null? uggarOption[0].value:localStorage.getItem('UserUgg'))
     setLanguage(() => localStorage.getItem('UserLanguage') === null? languageOption[0].value:localStorage.getItem('UserLanguage') )
-
-    localStorage.setItem('UserUgg', ugg)
-
-    localStorage.setItem('UserLanguage', language)
+    
 
   
   }, [])
+
+      useEffect(() => {
+      localStorage.setItem('UserUgg', ugg)
+    }, [ugg])
+  
+    useEffect(() => {
+      localStorage.setItem('UserLanguage', language)
+    }, [language])
 
 
 
