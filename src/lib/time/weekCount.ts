2c1a36d60748876655562c7ecd1017ffd95d0d2c
Ultimate hacker <70284330+Ultimate-hackman@users.noreply.github.com>
2021-01-25 10:09:28 +0100

    A,,a

--- src/lib/time/weekCount.ts
+++ src/lib/time/weekCount.ts
@@ -8,7 +8,7 @@ export default function weekFinder(weekday, hour) {
     let week: number = Math.floor(totalMonth(month) + day / 7)
     
 
-    return ( weekday + (hour/24) >= new Date().getDay() + (nowHour/24)) ? week : week +1
+    return ( weekday + (hour/24) >= new Date().getDay() + (nowHour/24)) ? week +1: week +2
 
 
 
