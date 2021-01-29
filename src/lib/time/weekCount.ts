d63a7343a3615b531f781690b7479ac0ebf7bce0
Ultimate hacker <70284330+Ultimate-hackman@users.noreply.github.com>
2021-01-29 17:52:07 +0100

    FIX

--- src/lib/time/weekCount.ts
+++ src/lib/time/weekCount.ts
@@ -9,7 +9,7 @@ export default function weekFinder(weekday, hour) {
     let nowHour: number = new Date().getHours();
     let week = dayjs().isoWeek()
     
-    return ( weekday + (hour/24) >= new Date().getDay() + (nowHour/24)) ? week : week +1
+    return ( weekday + (hour/24) > new Date().getDay() + (nowHour/24)) ? week : week +1
 
 
 
