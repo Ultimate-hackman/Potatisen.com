43596910797440a6ff7f31f9249cacad4496d407
Ultimate hacker <70284330+Ultimate-hackman@users.noreply.github.com>
2021-01-29 10:32:11 +0100

    Update

--- src/lib/calendar/stressPT.ts
+++ src/lib/calendar/stressPT.ts
@@ -19,7 +19,7 @@ export default function stressPT(ugg, language, totalData, day) {
             
             time.push(distance) 
             if (x >= 0) {
-              totalPt += (1.2 ** x)
+              totalPt += (100 - x)
             }
           
         }
