69ab90cb8fc2354b35c1e57fff3dadbe789c48bf
Ultimate hacker <70284330+Ultimate-hackman@users.noreply.github.com>
2021-01-29 12:03:06 +0100

    Bruh

--- src/lib/calendar/stressPT.ts
+++ src/lib/calendar/stressPT.ts
@@ -19,7 +19,7 @@ export default function stressPT(ugg, language, totalData, day) {
             
             time.push(distance) 
             if (x >= 0) {
-              totalPt += (100 - x)
+              totalPt += x
             }
           
         }
