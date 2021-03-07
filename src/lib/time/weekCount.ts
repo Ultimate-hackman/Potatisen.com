77555e474e8d748ec273b1c9e0b4443ac389329f
Ultimate-hackman <70284330+Ultimate-hackman@users.noreply.github.com>
2021-03-07 20:18:44 +0100

    fix

--- src/lib/time/weekCount.ts
+++ src/lib/time/weekCount.ts
@@ -6,8 +6,6 @@ dayjs.extend(isoWeek);
 export default function weekFinder(weekday: number, hour: number): number {
   const nowHour: number = new Date().getHours();
   const week = dayjs().isoWeek();
-  const day = dayjs().locale("sv", {
-    weekStart: 1,
-  });
+  const day = dayjs().locale("sv");
   return (weekday + (hour / 24) >= day.date() + (nowHour / 24)) ? week : week + 1;
 }
