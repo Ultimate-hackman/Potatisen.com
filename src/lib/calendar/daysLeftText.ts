21b6d8f31b39f3ed56a302e1b8183026ef6e9199
Ultimate-hackman <70284330+Ultimate-hackman@users.noreply.github.com>
2021-03-29 07:35:03 +0200

    yet another fix

--- src/lib/calendar/daysLeftText.ts
+++ src/lib/calendar/daysLeftText.ts
@@ -1,28 +1,22 @@
 import dayjs from "dayjs";
 
 function daysLeftText(timestamp: any): string {
-  const daysLeft = dayjs(timestamp).diff(dayjs(), "day", true) < 1 ? 0 : Math.ceil(dayjs(timestamp).diff(dayjs(), "day", true));
-  console.log(daysLeft);
-  if (daysLeft > -2) {
-    switch (daysLeft) {
+  const daysLeft = dayjs(timestamp).diff(dayjs(), "day", true);
+  const daysLeftAdapted = daysLeft < 1 ? Math.floor(daysLeft) : Math.ceil(daysLeft);
+  if (daysLeftAdapted > -2) {
+    switch (daysLeftAdapted) {
       case -2:
         return "förrgår";
-        break;
       case -1:
         return "igår";
-        break;
       case 0:
         return "idag";
-        break;
       case 1:
         return "imorgon";
-        break;
       case 2:
         return "övermorgon";
-        break;
       default:
-        return `${daysLeft} dagar kvar`;
-        break;
+        return `${daysLeftAdapted} dagar kvar`;
     }
   }
 }
