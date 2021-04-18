c1bbadbc8066719aaa92e61be764a6fa7e27d53d
Ultimate-hackman <70284330+Ultimate-hackman@users.noreply.github.com>
2021-04-18 17:31:48 +0200

    Update news.tsx

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import GlobalStyle from "../theme/GlobalStyles";
 import NewsLetters from "../components/newsLetter/newsLetters";
@@ -7,11 +7,11 @@ import InfoPad from "../styles/infoPad";
 import Img from "../styles/img";
 import useData from "../lib/discordAPI/useData";
 import BigBtn from "../styles/bigBtn";
+
 const VeckoImg = styled(Img)`
 width: 25%;
 padding-top: 10vh;
 `;
-
 
 const Bullet = styled.li`
 list-style-type:none;
@@ -19,8 +19,7 @@ list-style-type:none;
 
 export default function Veckobrev(): JSX.Element {
   const news = useData("/api/news");
-  const letters: any[] = useData("/api/letters")
-
+  const letters: any[] = useData("/api/letters");
 
   return (
     <>
@@ -28,11 +27,11 @@ export default function Veckobrev(): JSX.Element {
       <GlobalStyle />
       <Header title="Veckobrev" />
       <VeckoImg src="https://cdn.discordapp.com/attachments/688322560957743190/786315067352154172/veckobrev.edcc5d03.png" />
-      <a href={letters[0]?.url}> 
-      <BigBtn>{letters[0]?.title}</BigBtn>
+      <a href={letters[0]?.url}>
+        <BigBtn>{letters[0]?.title}</BigBtn>
       </a>
       <InfoPad>
-      <h1><span role="img" aria-label="note">Nyheter 📰🖊️</span></h1>
+        <h1><span role="img" aria-label="note">Nyheter 📰🖊️</span></h1>
         {news.map((item) => (
           <Bullet>
             {" "}
